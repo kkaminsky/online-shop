bde01591394dceda27043fdc22b74528f025b81d
Konstantin <mr.kkaminsky@list.ru>
2020-03-21 13:29:38 +0500

    init commit

---
+++ src/main/kotlin/com/kkaminsky/onlineshop/OnlineShopApplication.kt
@@ -0,0 +1,11 @@
+package com.kkaminsky.onlineshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnlineShopApplication
+
+fun main(args: Array<String>) {
+	runApplication<OnlineShopApplication>(*args)
+}
